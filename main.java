339eb64d6b82c9a52c9132f0abed2d4820ef97db
Forbrig <vitorforbrig@gmail.com>
2017-04-24 04:11:12 -0300

    modificações do leitor de arquivos que não foram comitadas

--- main.java
+++ main.java
@@ -1,12 +1,14 @@
-import java.util.Scanner; //scanf
+import java.util.*;
+import java.io.*;
 
 class main {
-	private leitor_arq leitor;
-
-	public static void main (String[] args) {
-		this.leitor = new leitor_arq();
-
-
-
+	public static void main (String[] args) throws FileNotFoundException {
+		try {
+			leitor_arq leitor = new leitor_arq();
+		}
+		
+		catch (FileNotFoundException ex) {
+			System.out.println("arquivo não encontrado\n");
+    	}
 	}
 }
--- professor.java
+++ professor.java
@@ -1,24 +1,32 @@
 class professor {
-	String nome;
-	int	qt_horarios;
-	int horarios_nquer[]; //int horarios_nquer[qt_horarios];
+	String nome_prof;
+	int	qt_horarios; //quantidade de horarios que o professor n quer
+	int[] horarios_nquer; //int horarios_nquer[qt_horarios]; //horarios que o professor n quer
 	
-	public void set_nome (String nome) {
-		this.nome = nome;
+	public void set_nome_prof (String nome) {
+		this.nome_prof = nome;
 	}
 
-	public void set_qt_horarios (int qt_horarios) {
+	public String get_nome_prof () {
+		return this.nome_prof;
+	}
+
+	public void set_qt_hnquer (int qt_horarios) {
 		this.qt_horarios = qt_horarios;
 	}
 
-	public int get_qt_horarios () {
+	public int get_qt_hnquer () {
 		return this.qt_horarios;
 	}
 
-	public void set_horarios_nquer () {
-		for (int i = 0; i < this.get_qt_horarios(); i++) {
-			//this.horarios_nquer[i];
-		} 
+	public void set_hnquer (int[] aux) {
+		this.horarios_nquer = new int[get_qt_hnquer()];
+		for (int i = 0; i < this.get_qt_hnquer(); i++) {
+			this.horarios_nquer[i] = aux[i];
+		}
 	}
 
+	public int[] get_hnquer () {
+		return horarios_nquer;
+	}
 }
